fa3d248e7355b072d490ef18a440850e354746a3
Teresa Loafman <teresaloafman@CBL-L84128.local>
2023-12-18 11:35:27 -0700

    Added base styles

--- app/components/whatToChargeForm.tsx
+++ app/components/whatToChargeForm.tsx
@@ -2,38 +2,110 @@ import { Form } from "@remix-run/react";
 
 const WhatToChargeForm = () => {
   return (
-    <div>
-      WhatToChargeForm
-      <Form>
-        <div>
-          <div>
-            <label htmlFor="idealHourlyRate">Ideal Hourly Rate</label>
-            <input id="idealHourlyRate" name="idealHourlyRate" />
-          </div>
-          <div>
-            <label htmlFor="gigPayment">Gig Payment</label>
-            <input id="gigPayment" name="gigPayment" />
-          </div>
-          <div>
-            <label htmlFor="gigHours">Gig Hours (not including driving)</label>
-            <input id="gigHours" name="gigHours" />
-          </div>
-          <div>
-            <label htmlFor="gigDrivingTime">Driving </label>
-            <input id="gigDrivingTime" name="gigDrivingTime" />
-          </div>
-          <div>
-            <label htmlFor="babysittingHours">Babysitting Hours</label>
-            <input id="babysittingHours" name="babysittingHours" />
-          </div>
-          <div>
-            <label htmlFor="babysittingHourlyRate">
-              Babysitting HourlyRate
+    <div className="w-full">
+      <div className="text-center text-xl">What Should I Charge?</div>
+      <div className="w-full max-w-lg my-0 mx-auto">
+        <Form className="bg-white shadow-md rounded px-8 pt-6 pb-8 mb-4">
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="idealHourlyRate"
+            >
+              Ideal Hourly Rate
             </label>
-            <input id="babysittingHourlyRate" name="babysittingHourlyRate" />
+            <input
+              className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="idealHourlyRate"
+              name="idealHourlyRate"
+              type="number"
+              placeholder="$"
+            />
           </div>
-        </div>
-      </Form>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="gigPayment"
+            >
+              Gig Payment
+            </label>
+            <input
+              className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="gigPayment"
+              name="gigPayment"
+              type="number"
+              placeholder="$"
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="gigHours"
+            >
+              Gig Hours (not including driving)
+            </label>
+            <input
+              className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="gigHours"
+              name="gigHours"
+              type="number"
+              placeholder=""
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="gigDrivingTime"
+            >
+              Driving Time
+            </label>
+            <input
+              className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="gigDrivingTime"
+              name="gigDrivingTime"
+              type="number"
+              placeholder=""
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="babysittingHours"
+            >
+              Babysitting Hours
+            </label>
+            <input
+              className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="babysittingHours"
+              name="babysittingHours"
+              type="number"
+              placeholder=""
+            />
+          </div>
+          <div className="mb-6">
+            <label
+              className="block text-gray-700 text-sm font-bold mb-2"
+              htmlFor="babysittingHourlyRate"
+            >
+              Babysitting Hourly Rate
+            </label>
+            <input
+              className="shadow appearance-none border  rounded w-full py-2 px-3 text-gray-700 mb-3 leading-tight focus:outline-none focus:shadow-outline"
+              id="babysittingHourlyRate"
+              name="babysittingHourlyRate"
+              type="number"
+              placeholder="$"
+            />
+          </div>
+          <div className="flex items-center justify-between">
+            <button
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded focus:outline-none focus:shadow-outline"
+              type="button"
+            >
+              Submit
+            </button>
+          </div>
+        </Form>
+      </div>
     </div>
   );
 };
--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -16,22 +16,26 @@ export default function Index() {
 
   return (
     <div>
-      <h1>Gig Guider</h1>
-      <div>
+      <div className="mt-20">
+        <h1 className="text-4xl text-center">Gig Guider</h1>
         {!shouldITakeThis && !whatToCharge && (
-          <>
+          <div className="flex items-center justify-between w-1/2 my-0 mx-auto h-screen">
             <button
               onClick={() => {
                 toggleShouldITakeThis(!shouldITakeThis);
               }}
+              className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
             >
               Should I take this gig?
             </button>
 
-            <button onClick={() => toggleWhatToCharge(!whatToCharge)}>
+            <button
+              onClick={() => toggleWhatToCharge(!whatToCharge)}
+              className="bg-transparent hover:bg-blue-500 text-blue-700 font-semibold hover:text-white py-2 px-4 border border-blue-500 hover:border-transparent rounded"
+            >
               How much should I charge for this gig?
             </button>
-          </>
+          </div>
         )}
         {shouldITakeThis || whatToCharge ? (
           <button
@@ -39,6 +43,7 @@ export default function Index() {
               toggleShouldITakeThis(false);
               toggleWhatToCharge(false);
             }}
+            className="ml-10"
           >
             Back
           </button>
