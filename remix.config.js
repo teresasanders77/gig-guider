81a78501202fafc1bdbad7dc25a201d1ade3fd4c
Teresa Loafman <teresaloafman@CBL-L84128.local>
2023-12-18 09:58:05 -0700

    Initial repo setup

--- remix.config.js
+++ remix.config.js
@@ -1,8 +1,13 @@
-/** @type {import('@remix-run/dev').AppConfig} */
+// remix.config.js
+import { createRequire } from "module";
+
+const require = createRequire(import.meta.url);
+
 export default {
   ignoredRouteFiles: ["**/.*"],
   // appDirectory: "app",
   // assetsBuildDirectory: "public/build",
   // publicPath: "/build/",
   // serverBuildPath: "build/index.js",
+  plugins: [require("remix-tailwind")],
 };
