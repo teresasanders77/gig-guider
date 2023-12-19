6f76e89b63552359364bf8c7a6e1e0d60388f258
Teresa Loafman <teresaloafman@CBL-L84128.local>
2023-12-18 17:05:37 -0700

    Cleaned up code

--- app/routes/answer.$answer.tsx
+++ app/routes/answer.$answer.tsx
@@ -1,6 +1,5 @@
 import { json } from "@remix-run/node";
 import { useLoaderData, useSearchParams } from "@remix-run/react";
-import { useEffect, useState } from "react";
 
 interface Params {
   answer: string;
@@ -41,32 +40,20 @@ export const loader = async ({ params }: { params: Params }) => {
 
 const Answer = () => {
   const { response } = useLoaderData<typeof loader>();
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const idealHourlyRate = searchParams.get("idealHourlyRate");
-  console.log("idealHourlyRate", idealHourlyRate);
   const gigPayment = searchParams.get("gigPayment");
-  // console.log("gigPayment", gigPayment);
   const gigHours = searchParams.get("gigHours");
-  // console.log("gigHours", gigHours);
   const mileage = searchParams.get("mileage");
-  // console.log("mileage", mileage);
   const babysittingHours = searchParams.get("babysittingHours");
-  // console.log("babysittingHours", babysittingHours);
   const babysittingHourlyRate = searchParams.get("babysittingHourlyRate");
-  // console.log("babysittingHourlyRate", babysittingHourlyRate);
 
-  const [details, toggleDetails] = useState(false);
-
-  const scrollToElement = (id) => {
+  const scrollToElement = (id: string) => {
     const element = document.getElementById(id);
     if (element) {
       element.scrollIntoView({ behavior: "smooth" });
     }
   };
-
-  useEffect(() => {
-    details ? scrollToElement("details") : null;
-  }, [details]);
 
   return (
     <>
@@ -74,95 +61,94 @@ const Answer = () => {
         <h2 className="text-center mt-10 font-bold text-lg">{response}</h2>
         <button
           onClick={() => {
-            toggleDetails(true);
+            scrollToElement("details");
           }}
         >
           See Details
         </button>
       </div>
-      {details && (
-        <div className="h-screen text-center" id="details">
-          <h1 className="font-bold text-xl">Details</h1>
-          <h2>Here is what you entered:</h2>
-          <p>
-            Ideal Hourly Rate:
-            <span className="font-bold">${idealHourlyRate}</span>
-          </p>
-          <p>
-            Gig Payment:
-            <span className="font-bold">${gigPayment}</span>
-          </p>
-          <p>
-            Gig Hours:
-            <span className="font-bold">{gigHours} Hours</span>
-          </p>
-          <p>
-            Mileage:
-            <span className="font-bold">{mileage} Miles</span>
-          </p>
-          <p>
-            Babysitting Hours:
-            <span className="font-bold">{babysittingHours} Hours</span>
-          </p>
-          <p>
-            Babysitting Hourly Rate:
-            <span className="font-bold">${babysittingHourlyRate}</span>
-          </p>
 
-          <h2 className="mt-10">Here how we calculated:</h2>
-          <p>
-            Gas Cost (Mileage * 2 * 0.67 (IRS Mileage Rate)):
-            <span className="font-bold">
-              ${Number(Number(mileage) * 2) * 0.67}
-            </span>
-          </p>
-          <p>
-            Babysitting Cost (Babysitting Hours * Babysitting Hourly Rate):
-            <span className="font-bold">
-              ${Number(babysittingHours) * Number(babysittingHourlyRate)}
-            </span>
-          </p>
-          <p>
-            Total Cost (Gas Cost + Babysitting Cost):
-            <span className="font-bold">
-              $
-              {Number(Number(mileage) * 2) * 0.67 +
-                Number(babysittingHours) * Number(babysittingHourlyRate)}
-            </span>
-          </p>
-          <p>
-            Hopeful Income Pre Expense (Ideal Hourly Rate * Gig Hours):
-            <span className="font-bold">
-              ${Number(idealHourlyRate) * Number(gigHours)}
-            </span>
-          </p>
-          <p>
-            Hopeful Income Total (Hopeful Income Pre Expense + Total Cost):
-            <span className="font-bold">
-              $
-              {Number(idealHourlyRate) * Number(gigHours) +
-                (Number(Number(mileage) * 2) * 0.67 +
-                  Number(babysittingHours) * Number(babysittingHourlyRate))}
-            </span>
-          </p>
-          <p>
-            Difference (Gig Payment - Hopeful Income Total):
-            <span className="font-bold">
-              $
-              {Number(gigPayment) -
-                (Number(idealHourlyRate) * Number(gigHours) +
-                  (Number(Number(mileage) * 2) * 0.67 +
-                    Number(babysittingHours) * Number(babysittingHourlyRate)))}
-            </span>
-          </p>
-          <h2 className="mt-10">
-            To make this gig worth it, we recommend you charge:
+      <div className="h-screen text-center" id="details">
+        <h1 className="font-bold text-xl">Details</h1>
+        <h2>Here is what you entered:</h2>
+        <p>
+          Ideal Hourly Rate:
+          <span className="font-bold">${idealHourlyRate}</span>
+        </p>
+        <p>
+          Gig Payment:
+          <span className="font-bold">${gigPayment}</span>
+        </p>
+        <p>
+          Gig Hours:
+          <span className="font-bold">{gigHours} Hours</span>
+        </p>
+        <p>
+          Mileage:
+          <span className="font-bold">{mileage} Miles</span>
+        </p>
+        <p>
+          Babysitting Hours:
+          <span className="font-bold">{babysittingHours} Hours</span>
+        </p>
+        <p>
+          Babysitting Hourly Rate:
+          <span className="font-bold">${babysittingHourlyRate}</span>
+        </p>
+
+        <h2 className="mt-10">Here how we calculated:</h2>
+        <p>
+          Gas Cost (Mileage * 2 * 0.67 (IRS Mileage Rate)):
+          <span className="font-bold">
+            ${Number(Number(mileage) * 2) * 0.67}
+          </span>
+        </p>
+        <p>
+          Babysitting Cost (Babysitting Hours * Babysitting Hourly Rate):
+          <span className="font-bold">
+            ${Number(babysittingHours) * Number(babysittingHourlyRate)}
+          </span>
+        </p>
+        <p>
+          Total Cost (Gas Cost + Babysitting Cost):
+          <span className="font-bold">
+            $
+            {Number(Number(mileage) * 2) * 0.67 +
+              Number(babysittingHours) * Number(babysittingHourlyRate)}
+          </span>
+        </p>
+        <p>
+          Hopeful Income Pre Expense (Ideal Hourly Rate * Gig Hours):
+          <span className="font-bold">
+            ${Number(idealHourlyRate) * Number(gigHours)}
+          </span>
+        </p>
+        <p>
+          Hopeful Income Total (Hopeful Income Pre Expense + Total Cost):
+          <span className="font-bold">
+            $
             {Number(idealHourlyRate) * Number(gigHours) +
               (Number(Number(mileage) * 2) * 0.67 +
                 Number(babysittingHours) * Number(babysittingHourlyRate))}
-          </h2>
-        </div>
-      )}
+          </span>
+        </p>
+        <p>
+          Difference (Gig Payment - Hopeful Income Total):
+          <span className="font-bold">
+            $
+            {Number(gigPayment) -
+              (Number(idealHourlyRate) * Number(gigHours) +
+                (Number(Number(mileage) * 2) * 0.67 +
+                  Number(babysittingHours) * Number(babysittingHourlyRate)))}
+          </span>
+        </p>
+        <h2 className="mt-10">
+          To make this gig worth it, we recommend you charge: $
+          {Number(idealHourlyRate) * Number(gigHours) +
+            (Number(Number(mileage) * 2) * 0.67 +
+              Number(babysittingHours) * Number(babysittingHourlyRate))}
+        </h2>
+      </div>
     </>
   );
 };
--- app/routes/charge.tsx
+++ app/routes/charge.tsx
@@ -2,22 +2,16 @@ import { useSearchParams } from "@remix-run/react";
 import { useEffect, useState } from "react";
 
 const Charge = () => {
-  const [searchParams, setSearchParams] = useSearchParams();
+  const [searchParams] = useSearchParams();
   const idealHourlyRate = searchParams.get("idealHourlyRate");
-  //   console.log("idealHourlyRate", idealHourlyRate);
   const gigHours = searchParams.get("gigHours");
-  // console.log("gigHours", gigHours);
   const mileage = searchParams.get("mileage");
-  // console.log("mileage", mileage);
   const babysittingHours = searchParams.get("babysittingHours");
-  // console.log("babysittingHours", babysittingHours);
   const babysittingHourlyRate = searchParams.get("babysittingHourlyRate");
-  // console.log("babysittingHourlyRate", babysittingHourlyRate);
 
-  const [details, toggleDetails] = useState(false);
   const [total, setTotal] = useState(0);
 
-  const scrollToElement = (id) => {
+  const scrollToElement = (id: string) => {
     const element = document.getElementById(id);
     if (element) {
       element.scrollIntoView({ behavior: "smooth" });
@@ -25,22 +19,19 @@ const Charge = () => {
   };
 
   useEffect(() => {
-    details ? scrollToElement("details") : null;
-  }, [details]);
-
-  useEffect(() => {
     const gasCost = Number(Number(mileage) * 2) * 0.67;
-    // console.log("gasCost: ", gasCost);
     const babysittingCost =
       Number(babysittingHours) * Number(babysittingHourlyRate);
-    // console.log("babysittingCost: ", babysittingCost);
     const totalCost = gasCost + babysittingCost;
-    // console.log("totalCost: ", totalCost);
     const hopefulIncomePreExpense = Number(idealHourlyRate) * Number(gigHours);
-    // console.log("hopefulIncomePreExpense: ", hopefulIncomePreExpense);
     setTotal(hopefulIncomePreExpense + totalCost);
-    // console.log("hopefulIncomeTotal: ", hopefulIncomeTotal);
-  }, []);
+  }, [
+    babysittingHourlyRate,
+    babysittingHours,
+    gigHours,
+    idealHourlyRate,
+    mileage,
+  ]);
 
   return (
     <>
@@ -50,75 +41,73 @@ const Charge = () => {
         </h2>
         <button
           onClick={() => {
-            toggleDetails(true);
+            scrollToElement("details");
           }}
         >
           See Details
         </button>
       </div>
-      {details && (
-        <div className="h-screen text-center" id="details">
-          <h1 className="font-bold text-xl">Details</h1>
-          <h2>Here is what you entered:</h2>
-          <p>
-            Ideal Hourly Rate:
-            <span className="font-bold">${idealHourlyRate}</span>
-          </p>
-          <p>
-            Gig Hours:
-            <span className="font-bold">{gigHours} Hours</span>
-          </p>
-          <p>
-            Mileage:
-            <span className="font-bold">{mileage} Miles</span>
-          </p>
-          <p>
-            Babysitting Hours:
-            <span className="font-bold">{babysittingHours} Hours</span>
-          </p>
-          <p>
-            Babysitting Hourly Rate:
-            <span className="font-bold">${babysittingHourlyRate}</span>
-          </p>
+      <div className="h-screen text-center" id="details">
+        <h1 className="font-bold text-xl">Details</h1>
+        <h2>Here is what you entered:</h2>
+        <p>
+          Ideal Hourly Rate:
+          <span className="font-bold">${idealHourlyRate}</span>
+        </p>
+        <p>
+          Gig Hours:
+          <span className="font-bold">{gigHours} Hours</span>
+        </p>
+        <p>
+          Mileage:
+          <span className="font-bold">{mileage} Miles</span>
+        </p>
+        <p>
+          Babysitting Hours:
+          <span className="font-bold">{babysittingHours} Hours</span>
+        </p>
+        <p>
+          Babysitting Hourly Rate:
+          <span className="font-bold">${babysittingHourlyRate}</span>
+        </p>
 
-          <h2 className="mt-10">Here how we calculated:</h2>
-          <p>
-            Gas Cost (Mileage * 2 * 0.67 (IRS Mileage Rate)):
-            <span className="font-bold">
-              ${Number(Number(mileage) * 2) * 0.67}
-            </span>
-          </p>
-          <p>
-            Babysitting Cost (Babysitting Hours * Babysitting Hourly Rate):
-            <span className="font-bold">
-              ${Number(babysittingHours) * Number(babysittingHourlyRate)}
-            </span>
-          </p>
-          <p>
-            Total Cost (Gas Cost + Babysitting Cost):
-            <span className="font-bold">
-              $
-              {Number(Number(mileage) * 2) * 0.67 +
-                Number(babysittingHours) * Number(babysittingHourlyRate)}
-            </span>
-          </p>
-          <p>
-            Hopeful Income Pre Expense (Ideal Hourly Rate * Gig Hours):
-            <span className="font-bold">
-              ${Number(idealHourlyRate) * Number(gigHours)}
-            </span>
-          </p>
-          <p>
-            Hopeful Income Total (Hopeful Income Pre Expense + Total Cost):
-            <span className="font-bold">
-              $
-              {Number(idealHourlyRate) * Number(gigHours) +
-                (Number(Number(mileage) * 2) * 0.67 +
-                  Number(babysittingHours) * Number(babysittingHourlyRate))}
-            </span>
-          </p>
-        </div>
-      )}
+        <h2 className="mt-10">Here how we calculated:</h2>
+        <p>
+          Gas Cost (Mileage * 2 * 0.67 (IRS Mileage Rate)):
+          <span className="font-bold">
+            ${Number(Number(mileage) * 2) * 0.67}
+          </span>
+        </p>
+        <p>
+          Babysitting Cost (Babysitting Hours * Babysitting Hourly Rate):
+          <span className="font-bold">
+            ${Number(babysittingHours) * Number(babysittingHourlyRate)}
+          </span>
+        </p>
+        <p>
+          Total Cost (Gas Cost + Babysitting Cost):
+          <span className="font-bold">
+            $
+            {Number(Number(mileage) * 2) * 0.67 +
+              Number(babysittingHours) * Number(babysittingHourlyRate)}
+          </span>
+        </p>
+        <p>
+          Hopeful Income Pre Expense (Ideal Hourly Rate * Gig Hours):
+          <span className="font-bold">
+            ${Number(idealHourlyRate) * Number(gigHours)}
+          </span>
+        </p>
+        <p>
+          Hopeful Income Total (Hopeful Income Pre Expense + Total Cost):
+          <span className="font-bold">
+            $
+            {Number(idealHourlyRate) * Number(gigHours) +
+              (Number(Number(mileage) * 2) * 0.67 +
+                Number(babysittingHours) * Number(babysittingHourlyRate))}
+          </span>
+        </p>
+      </div>
     </>
   );
 };
